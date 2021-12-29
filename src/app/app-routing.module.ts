aa1663d9e3f168a8c15bed3b7fa2a7462bc5f7c3
Fredrik Johansson Torneus <fredrik.johansson-torneus@globalconnect.se>
2021-12-29 11:22:45 +0100

    Set default routing to series-counter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeriesCounterComponent } from './series-counter/series-counter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'series-counter', component: SeriesCounterComponent
+  },
+  {
+    path: '**', redirectTo: 'series-counter'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/series-counter/series-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-series-counter',
+  templateUrl: './series-counter.component.html',
+  styleUrls: ['./series-counter.component.scss']
+})
+export class SeriesCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
